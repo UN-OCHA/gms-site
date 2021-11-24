67fa31d2ea2b88b6d869af29a502ce085aab2b90
Jigar Patel <Jigar.Patel@trigyn.com>
2021-11-24 14:47:17 +0530

    Execution time increase

--- docker/settings.php
+++ docker/settings.php
@@ -47,5 +47,5 @@ $settings['file_public_path'] = 'sites/default/files';
 $settings['file_private_path'] = 'sites/default/files/private';
 ini_set('allow_url_fopen', 'On');
 ini_set('memory_limit', '-1');
-ini_set('max_execution_time ', '1000');
+ini_set('max_execution_time', '1000');
 ini_set("pcre.backtrack_limit", "5000000");
