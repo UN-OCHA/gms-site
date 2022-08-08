af60441595f97f21db044db04d58e39e8efb05a4
Nitesh <nitesh.pawar@trigyn.com>
2022-08-08 14:02:20 +0530

    Added social auth role module and custom module for gms secure role

--- html/modules/custom/gms_secure_role/src/EventSubscriber/GmsOchaEventSubscriber.php
+++ html/modules/custom/gms_secure_role/src/EventSubscriber/GmsOchaEventSubscriber.php
@@ -74,7 +74,7 @@ class GmsOchaEventSubscriber implements EventSubscriberInterface {
    *   The user event object.
    */
   public function showPopup(UserEvent $event) {
-    $account = $event->getUser();
+    //$account = $event->getUser();
 
   }
 
